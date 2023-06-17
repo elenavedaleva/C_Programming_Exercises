73d65a2e7b10ca0ddcf1a909a7cf7ee8aafd81df
Elena Vedaleva <elenavedaleva@gmail.com>
2023-06-16 20:46:38 -0400

    add 15_leap_year

---
+++ 15_leap_year/leap_year.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_leap_year(int year);
+
+int main(void) {
+  for (int y = 1900; y <= 2100; y++) {
+    if (is_leap_year(y)) {
+      printf("%d  -> leap year\n", y);
+    } else {
+      printf("%d\n", y);
+    }
+  }
+
+  return 0;
+}
+
+bool is_leap_year(int year) {
+  if (year % 4 != 0) return false;
+  else if (year % 100 != 0) return true;
+  else if (year % 400 != 0) return false;
+  else return true;
+}
